ff88ebe783513917b550ada5a95be9664a3bfaba
LindaWang7 <48342363+LindaWang7@users.noreply.github.com>
2022-02-23 00:50:35 -0500

    load video

--- chapter_1.py
+++ chapter_1.py
@@ -1,7 +1,15 @@
 import cv2
 
 #show Images
-img = cv2.imread("Resources/pikachu.png")
-cv2.imshow("Output", img)
-cv2.waitKey(0)
+# img = cv2.imread("Resources/pikachu.png")
+# cv2.imshow("Output", img)
+# cv2.waitKey(0)
 
+#Show Video
+cap = cv2.VideoCapture("Resources/pikachu.mp4")
+
+while True:
+    success, img = cap.read()
+    cv2.imshow("Video", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
